72db707d234d42a72b6e8e25869ece8e5445ee8a
rifan <tararifan93@gmail.com>
2025-08-15 21:35:56 +0700

    add feature event click ./simple-gallery-with-dom
    - **simple-gallery-with-dom**: This is a simple gallery that uses DOM manipulation to display images. It

---
+++ javascript/DOM-exercise/simple-gallery-with-dom/script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+const jumbo = container.querySelector('.jumbo-thumbnail')
+
+container.addEventListener('click', function(e) {
+    if (e.target.className == 'thumbnail') {
+        jumbo.src = e.target.src;
+    }
+})

62176a34a35b31f71d3e2b9a84dc63b69f0f6303
rifan <tararifan93@gmail.com>
2025-08-16 13:05:11 +0700

    Add feature fadeIn for ./simple-gallery-with-dom
    - **feat:** Add feature fadeIn for ./simple-gallery-with-dom
    - when click image, the jumbo image will fade in with simple animation

--- javascript/DOM-exercise/simple-gallery-with-dom/script.js
+++ javascript/DOM-exercise/simple-gallery-with-dom/script.js
@@ -4,5 +4,9 @@ const jumbo = container.querySelector('.jumbo-thumbnail')
 container.addEventListener('click', function(e) {
     if (e.target.className == 'thumbnail') {
         jumbo.src = e.target.src;
+        jumbo.classList.add('fade')
+        setTimeout(function() {
+            jumbo.classList.remove('fade')
+        }, 300)
     }
 })

10250c5f242f9444ede8155c420db238514318f2
rifan <tararifan93@gmail.com>
2025-08-16 13:43:05 +0700

    Add feature active for ./simple-image-with-dom
    - add feature active for ./simple-image-with-dom
    - modifier hover for image
    - resolf jumbo-image

--- javascript/DOM-exercise/simple-gallery-with-dom/script.js
+++ javascript/DOM-exercise/simple-gallery-with-dom/script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const jumbo = container.querySelector('.jumbo-thumbnail')
+const thubnails = container.querySelectorAll('.thumbnail')
 
 container.addEventListener('click', function(e) {
     if (e.target.className == 'thumbnail') {
@@ -8,5 +9,10 @@ container.addEventListener('click', function(e) {
         setTimeout(function() {
             jumbo.classList.remove('fade')
         }, 300)
+
+        thubnails.forEach(function(thumbnail) {
+            thumbnail.className = 'thumbnail';
+        })
+        e.target.classList.add('active')
     }
 })

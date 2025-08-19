5db3ef20335572831a3a910c9bd1e3b824de966d
rifan <tararifan93@gmail.com>
2025-08-19 11:35:30 +0700

    Update project simpleGameTicTacToe in ./react 0.4
    - create rules winner & draw
    - modified App.jsx & index.css

--- javascript/react/simple-game-ticTacToe/src/App.jsx
+++ javascript/react/simple-game-ticTacToe/src/App.jsx
@@ -6,25 +6,66 @@ function Square ({value, onSquereClick}) {
   return <button className="square" onClick={onSquereClick}>{value}</button>
 }
 
+function calculateWinner(squeres) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    // const a = lines[i][0]; // 0
+    // const b = lines[i][1]; // 1
+    // const c = lines[i][2]; // 2
+    const [a,b,c] = lines[i];
+
+    if (squeres[a] && squeres[a] === squeres[b] && squeres[a] === squeres[c]) {
+      return squeres[a];
+    }
+  }
+
+  return false;
+}
+
 export default function Board() {
   const [squeres, setSqueres] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleSquereClick(i) {
-    if (squeres[i]) return;
+    if (squeres[i] || calculateWinner(squeres)) return;
     const nextSqueres = squeres.slice();
     // if(xIsNext) {
     //   nextSqueres[i] = 'x';
     // } else {
     //   nextSqueres[i] = 'o';
     // }
-    nextSqueres[i] = xIsNext ? 'x' : 'o';
+    nextSqueres[i] = xIsNext ? 'X' : 'O';
     setSqueres(nextSqueres);
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squeres);
+  console.log(winner)
+  let status = '';
+  if (winner) {
+    status = `The Winner is : ${winner}`;
+  } else {
+    const allSquaresFull = squeres.every(Square => Square !== null);
+    if (allSquaresFull) {
+      status = `Draw!`
+    } else {
+      status = `Next Player is : ${xIsNext ? 'X' : 'O'}`;
+    }
+  } 
+
   return (
     <>
+    <div className="status">{status}</div>
     <div className="board">
       
       <Square value={squeres[0]} onSquereClick={() => handleSquereClick(0)} />

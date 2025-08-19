1ab2ab75dd8dfde3d00637a8030bda86d43854f2
rifan <tararifan93@gmail.com>
2025-08-19 09:06:34 +0700

    Update project simpleGameTicTacToe in ./react
    - create a game board & make it clickable with a temporary value of 'x'
    - modified file index.css & App.jsx

--- javascript/react/simple-game-ticTacToe/src/App.jsx
+++ javascript/react/simple-game-ticTacToe/src/App.jsx
@@ -1,11 +1,33 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
-function App() {
+
+function Square () {
+  const [value, setValue] = useState(null);
+
+  function handleClick () {
+    setValue('x')
+  }
+
+  return <button className="square" onClick={handleClick}>{value}</button>
+}
+
+export default function Board() {
 
   return (
     <>
+    <div className="board">
+      
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+
+    </div>
     </>
   )
-}
-
-export default App
+};

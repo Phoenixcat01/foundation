b38f19aad6b11ec815006fc1b7bb4da30c83565a
rifan <tararifan93@gmail.com>
2025-08-19 10:05:47 +0700

    Update project simpleGameTicTacToe in ./react 0.2
    - create lifting state up & feature immutability
    - modified App.jsx

--- javascript/react/simple-game-ticTacToe/src/App.jsx
+++ javascript/react/simple-game-ticTacToe/src/App.jsx
@@ -1,31 +1,33 @@
 import { useState } from 'react'
 
 
-function Square () {
-  const [value, setValue] = useState(null);
+function Square ({value, onSquereClick}) {
 
-  function handleClick () {
-    setValue('x')
-  }
-
-  return <button className="square" onClick={handleClick}>{value}</button>
+  return <button className="square" onClick={onSquereClick}>{value}</button>
 }
 
 export default function Board() {
+  const [squeres, setSqueres] = useState(Array(9).fill(null))
+
+  function handleSquereClick(i) {
+    const nextSqueres = squeres.slice();
+    nextSqueres[i] = 'x';
+    setSqueres(nextSqueres)
+  }
 
   return (
     <>
     <div className="board">
       
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squeres[0]} onSquereClick={() => handleSquereClick(0)} />
+      <Square value={squeres[1]} onSquereClick={() => handleSquereClick(1)}/>
+      <Square value={squeres[2]} onSquereClick={() => handleSquereClick(2)}/>
+      <Square value={squeres[3]} onSquereClick={() => handleSquereClick(3)}/>
+      <Square value={squeres[4]} onSquereClick={() => handleSquereClick(4)}/>
+      <Square value={squeres[5]} onSquereClick={() => handleSquereClick(5)}/>
+      <Square value={squeres[6]} onSquereClick={() => handleSquereClick(6)}/>
+      <Square value={squeres[7]} onSquereClick={() => handleSquereClick(7)}/>
+      <Square value={squeres[8]} onSquereClick={() => handleSquereClick(8)}/>
 
     </div>
     </>

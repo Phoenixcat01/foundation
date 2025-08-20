ca98d3d61f693bf885f7c23d75e4dcc1849a1419
rifan <tararifan93@gmail.com>
2025-08-20 20:37:51 +0700

    Update simple game ticTacToe with react
    refactoring step 1 : commentared old code

--- javascript/react/simple-game-ticTacToe/src/App.jsx
+++ javascript/react/simple-game-ticTacToe/src/App.jsx
@@ -28,14 +28,9 @@ function Square ({value, onSquereClick}) {
 function calculateWinner(squeres) {
   // All possible winning lines
   const lines = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
+    [0,1,2], [3,4,5], [6,7,8], // horizontal lines
+    [0,3,6], [1,4,7], [2,5,8], // vertikal lines
+    [0,4,8], [2,4,6], //diagonal lines
   ];
 
   // Loop to check each winning line
@@ -53,9 +48,9 @@ function calculateWinner(squeres) {
     }
   }
 
-  // If there is no winner, return false.
+  // If there is no winner, return null.
   // Note: Returning `null` is more common in React for "nothing".
-  return false;
+  return null;
 }
 
 // Functional component for the game board
@@ -88,18 +83,14 @@ function Board({xIsNext, squeres, onPlay}) {
   // This code will print the result to the browser console
   console.log(winner)
 
-  let status = '';
+  let status;
   // Logic to determine game status
   if (winner) {
     status = `The Winner is : ${winner}`;
   } else {
     // Check if all boxes are filled for Draw status
     const allSquaresFull = squeres.every(Square => Square !== null);
-    if (allSquaresFull) {
-      status = `Draw!`
-    } else {
-      status = `Next Player is : ${xIsNext ? 'X' : 'O'}`;
-    }
+    allSquaresFull ? status = `Draw!` : status = `Next Player is : ${xIsNext ? 'X' : 'O'}`;
   } 
 
   // Render the game board UI
@@ -108,8 +99,15 @@ function Board({xIsNext, squeres, onPlay}) {
     <div className="status">{status}</div>
     <div className="board">
       
+      {squeres.map((value, i) => (
+        <Square 
+          key={i}
+          value={value}
+          onSquereClick={() => handleSquereClick(i)}/>
+      ))}
+
       {/* Using JSX to render each box manually */}
-      <Square value={squeres[0]} onSquereClick={() => handleSquereClick(0)} />
+      {/* <Square value={squeres[0]} onSquereClick={() => handleSquereClick(0)} />
       <Square value={squeres[1]} onSquereClick={() => handleSquereClick(1)}/>
       <Square value={squeres[2]} onSquereClick={() => handleSquereClick(2)}/>
       <Square value={squeres[3]} onSquereClick={() => handleSquereClick(3)}/>
@@ -117,7 +115,7 @@ function Board({xIsNext, squeres, onPlay}) {
       <Square value={squeres[5]} onSquereClick={() => handleSquereClick(5)}/>
       <Square value={squeres[6]} onSquereClick={() => handleSquereClick(6)}/>
       <Square value={squeres[7]} onSquereClick={() => handleSquereClick(7)}/>
-      <Square value={squeres[8]} onSquereClick={() => handleSquereClick(8)}/>
+      <Square value={squeres[8]} onSquereClick={() => handleSquereClick(8)}/> */}
 
     </div>
     </>
@@ -126,40 +124,37 @@ function Board({xIsNext, squeres, onPlay}) {
 
 // The main component of the application, which manages the overall game state
 export default function Game() {
-  // State to track player turns
-  const [xIsNext, setXIsNext] = useState(true);
   // State to save game board history
   const [history, setHistory] = useState([Array(9).fill(null)])
   // State to track the step currently being viewed
   const [currentMove, setCurrentMove] = useState(0)
   // Get the current board state from history based on currentMove
   const currentSqueres = history[currentMove];
+  // Determine turns based on parity of move number
+  const xIsNext = currentMove % 2 === 0;
 
   // Function to return to the previous step
   function jumpTo (nextMove) {
     setCurrentMove(nextMove);
-    // Determine turns based on parity of move number
-    setXIsNext(nextMove % 2 === 0);
   }
 
   // Function called from Board to update the game state
   function handlePlay(nextSqueres) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSqueres]
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSqueres];
     setHistory(nextHistory);
     // Update current step to last step
     setCurrentMove(nextHistory.length -1)
-    // Change player's turn
-    setXIsNext(!xIsNext);
   }
 
   // Create a list of buttons for the step history
   const moves = history.map((squeres, move) => {
-    let description = '';
-    if(move > 0) {
-      description = `Move to # ${move} !`;
-    } else {
-      description = `Go to Start!`;
-    }
+    let description;
+    move > 0 ? description = `Move to # ${move} !` : description = `Go to Start!`;
+    // if(move > 0) {
+    //   description = `Move to # ${move} !`;
+    // } else {
+    //   description = `Go to Start!`;
+    // }
 
     return (
       <li key={move}>
@@ -172,7 +167,6 @@ export default function Game() {
   function handleRestart () {
     setHistory([Array(9).fill(null)]);
     setCurrentMove(0);
-    setXIsNext(true);
   }
 
   // Renders the main UI of the application
